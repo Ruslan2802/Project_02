709928e382a807951b07485a114d3e6f17fdb0e1
Ruslan2802 <rus28021983@gmail.comr>
2025-08-20 12:11:01 +0300

    добавлено отображение времени напоминания

--- Reminder.py
+++ Reminder.py
@@ -21,6 +21,7 @@ def set():
             print(dt)
             t = dt.timestamp()
             print(t)
+            label.config(text=f'Напоминание на {hour:02}:{minute:02}')
         except Exception as e:
             mb.showerror('Ошибка!', f'Произошла ошибка {e}')
             
